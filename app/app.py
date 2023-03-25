b79509b4395f2d3fc313b7b8d16108919f5c7463
juanjomoreno11 <castanomorenoj@gmail.com>
2023-03-25 18:58:24 -0400

    testing

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return 'Welcome to our home page'
+    return 'Welcome to the template for the Open Door portal'
 
 if __name__ == '__main__':
     app.run()
