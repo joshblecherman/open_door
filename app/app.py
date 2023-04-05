fd52aca47d1abb8d64cd91ca1eda0e671b4ad8f1
juanjomoreno11 <castanomorenoj@gmail.com>
2023-04-05 15:26:58 -0400

    Home page completed

--- app/app.py
+++ app/app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return render_template('home.html')
+    # test_db()
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
