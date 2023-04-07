5be8f9a1123bf504f5c74398a4ff63c379b4b223
juanjomoreno11 <castanomorenoj@gmail.com>
2023-04-07 16:50:55 -0400

    Made templates for all basic pages and made a function to check for tabs redirection. Still not completely working

--- app/app.py
+++ app/app.py
@@ -3,41 +3,69 @@ from od_utils import db
 
 app = Flask(__name__)
 
+def check_main_tabs():
+    if request.form.get('my_profile') == 'My Profile':
+        return 'profile_page'
+        
+    elif request.form.get('student_events') == 'Student Events':
+        return 'student_events_page'
+    
+    elif request.form.get('fun_spots') == 'Fun Spots':
+        return 'fun_spots'
+    
+    elif request.form.get('happening_in_nyc') == 'Happening in NYC':
+        return 'happening_in_nyc'
+    else:
+        return False
+
+
 @app.route('/', methods=["GET", "POST"])
 def home_page():
-    db.test_db()
+    # db.test_db()
     if request.method == 'POST':
-        if request.form.get('my_profile') == 'My Profile':
-            return redirect(url_for('profile_page'))
-        
-        elif  request.form.get('student_events') == 'Student Events':
-            return redirect(url_for('profile_page'))
-        
-        elif request.form.get('fun_spots') == 'Fun Spots':
-            return redirect(url_for('profile_page'))
-        
-        elif request.form.get('happening_in_nyc') == 'Happening in NYC':
+        tabs = check_main_tabs()
+        if tabs:
             return redirect(url_for('profile_page'))
     else:
         return render_template('home.html')
 
+
 @app.route('/myprofile', methods=["GET", "POST"])
 def profile_page():
     if request.method == 'POST':
-        if request.form.get('my_profile') == 'My Profile':
-            return redirect(url_for('home_page'))
-        
-        elif  request.form.get('student_events') == 'Student Events':
-            return redirect(url_for('home_page'))
-        
-        elif request.form.get('fun_spots') == 'Fun Spots':
-            return redirect(url_for('home_page'))
-        
-        elif request.form.get('happening_in_nyc') == 'Happening in NYC':
+        tabs = check_main_tabs()
+        if tabs:
             return redirect(url_for('home_page'))
     else:
         return render_template('profile.html')
 
+
+@app.route('/studentevents', methods=["GET", "POST"])
+def student_events_page():
+    if request.method == 'POST':
+        tabs = check_main_tabs()
+        if tabs:
+            return redirect(url_for('home_page'))
+    else:
+        return render_template('profile.html')
+
+@app.route('/funspots', methods=["GET", "POST"])
+def fun_spots_page():
+    if request.method == 'POST':
+        tabs = check_main_tabs()
+        if tabs:
+            return redirect(url_for('home_page'))
+    else:
+        return render_template('profile.html')
+    
+@app.route('/happeninginnyc', methods=["GET", "POST"])
+def happening_in_nyc_page():
+    if request.method == 'POST':
+        tabs = check_main_tabs()
+        if tabs:
+            return redirect(url_for('home_page'))
+    else:
+        return render_template('profile.html')
 
 if __name__ == '__main__':
     app.run()
