6d1d3afa7ca6fe180644ec56d33634dc22f13c45
David Son <son.david.b@gmail.com>
2023-04-12 03:18:03 -0400

    Changed code to be consistent with SQLAlchemy

--- app/od_utils/db.py
+++ app/od_utils/db.py
@@ -1,17 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request
+from flask import Flask, current_app
 import uuid
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'postgresql://postgres:opendoor@localhost:5432/postgres'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app = current_app
+db = SQLAlchemy()
 
 class Profiles(db.Model):
     __tablename__ = "profiles"
     
-    user_id = db.Column(db.String(10), primary_key=True)
+    net_id = db.Column(db.String(10), primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    preferred_name = db.Column(db.String(50))
+    middle_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50), nullable=False)
     dorm = db.Column(db.String(50))
     major = db.Column(db.String(50))
     description = db.Column(db.String(2000))
@@ -19,10 +20,10 @@ class Profiles(db.Model):
 class Users(db.Model):
     __tablename__ = "users"
     
-    user_id = db.Column(db.String(10), primary_key=True)
+    net_id = db.Column(db.String(10), primary_key=True)
     dorm = db.Column(db.String(100), nullable=False)
     profile = db.Column(db.String(10),
-                        db.ForeignKey("profiles.user_id"),
+                        db.ForeignKey("profiles.net_id"),
                         nullable = False)
 
 class Activities(db.Model):
@@ -32,7 +33,7 @@ class Activities(db.Model):
     title = db.Column(db.String(50), nullable=False)
     place = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(2000))
-    time = db.Column(db.DateTime, nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False)
     fee = db.Column(db.Integer, nullable=False)
     url = db.Column(db.String(255))
     img = db.Column(db.LargeBinary)
@@ -47,30 +48,44 @@ class StudentEvents(db.Model):
                    primary_key=True)
     notes = db.Column(db.String(2000))
 
-class TicketmasterStaging(db.Model):
-    __tablename__ = "ticketmaster_staging"
+class Ticketmaster(db.Model):
+    __tablename__ = "ticketmaster"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    time = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    img_url = db.Column(db.String(255))
+    url = db.Column(db.String(255), nullable=False)
 
+# Ignores any conflicts with existing tables.
+# Bear in mind any tables with the same name will not be made.
+# Any table structure changes must be done via the database's CLI.
+# Alternatively, drop all tables and recreate them.
 def create_tables():
     with app.app_context():
         db.create_all()
 
-def remove_all_tables():
+# USE SPARINGLY DO NOT EVER USE THIS IN PRODUCTION
+# (So why did I make this? I dunno...)
+def drop_all_tables():
+    tables = [Users, Ticketmaster, StudentEvents, Activities, Profiles]
+
     with app.app_context():
-        Users.__table__.drop(db.engine)
-        TicketmasterStaging.__table__.drop(db.engine)
-        StudentEvents.__table__.drop(db.engine)
-        Activities.__table__.drop(db.engine)
-        Profiles.__table__.drop(db.engine)
+        inspect = db.inspect(db.engine)
+        for table in tables:
+            if inspect.has_table(table.__tablename__):
+                table.__table__.drop(db.engine)
 
 def test_db():
     with app.app_context():
         create_tables()
-        remove_all_tables()
-        print(db.metadata.tables.__dir__())
 
-if __name__ == '__main__':
-    test_db()
+# Just for testing
+# if __name__ == '__main__':
+#     app = Flask(__name__)
+#     app.config['SQLALCHEMY_DATABASE_URI'] = \
+#         'postgresql://postgres:opendoor@localhost:5432/postgres'
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#     db.init_app(app)
+#     test_db()
