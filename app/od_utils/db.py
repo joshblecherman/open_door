85e87762a6eea4efad04e4e967927dc82ca8b4c4
David Son <son.david.b@gmail.com>
2023-04-17 05:20:08 -0400

    Added db interactions

--- app/od_utils/db.py
+++ app/od_utils/db.py
@@ -1,5 +1,15 @@
+"""Utility module for reading from and modifying database.
+
+Note: When SQLAlchemy class (Type[db.Model]) is referred to as a type,
+it refers to a class name such as Profiles or Users.
+When SQLAlchemy object (db.Model) is referred to as a type,
+it refers to an object pertaining to a class,
+such as Profiles(net_id="jd1234", first_name="John", last_name="Doe")
+"""
+
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, current_app
+from typing import Type
 import uuid
 
 app = current_app
@@ -21,7 +31,7 @@ class Users(db.Model):
     __tablename__ = "users"
     
     net_id = db.Column(db.String(10), primary_key=True)
-    dorm = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     profile = db.Column(db.String(10),
                         db.ForeignKey("profiles.net_id"),
                         nullable = False)
@@ -58,17 +68,23 @@ class Ticketmaster(db.Model):
     img_url = db.Column(db.String(255))
     url = db.Column(db.String(255), nullable=False)
 
-# Ignores any conflicts with existing tables.
-# Bear in mind any tables with the same name will not be made.
-# Any table structure changes must be done via the database's CLI.
-# Alternatively, drop all tables and recreate them.
 def create_tables():
+    """Ignores any conflicts with existing tables.
+    Bear in mind any tables with the same name will not be made.
+    Any table structure changes must be done via the database's CLI.
+    Alternatively, drop all tables and recreate them.
+    
+    return: None
+    """
     with app.app_context():
         db.create_all()
 
-# USE SPARINGLY DO NOT EVER USE THIS IN PRODUCTION
-# (So why did I make this? I dunno...)
 def drop_all_tables():
+    """USE THIS SPARINGLY NEVER USE THIS IN PRODUCTION
+    (So why did I make this? I dunno...)
+    
+    return: None
+    """
     tables = [Users, Ticketmaster, StudentEvents, Activities, Profiles]
 
     with app.app_context():
@@ -77,15 +93,158 @@ def drop_all_tables():
             if inspect.has_table(table.__tablename__):
                 table.__table__.drop(db.engine)
 
+def add(data: db.Model, commit=True, overwrite=False):
+    """Inserts model into its respective table.
+    
+    :param data: SQLAlchemy object to be inserted
+    :param commit: Pushes changes immediately to database if True.
+    :param overwrite: If True, overwrite existing
+    data if primary key is found in table
+    
+    :return: None
+    """
+    with app.app_context():
+        if overwrite:
+            db.session.merge(data)
+        else:
+            db.session.add(data)
+
+        if commit:
+            db.session.commit()
+
+def get_with_pk(table_class: Type[db.Model], pk: any) -> db.Model:
+    """Retrieves the single model corresponding to the primary key.
+    
+    :param table_class: SQLAlchemy class to get data from
+    :param pk: Primary key used to find desired row.
+    
+    :return: SQLAlchemy model if found, None otherwise."""
+    with app.app_context():
+        return db.session.get(table_class, pk)
+
+def get_with_attributes(table_class: Type[db.Model], attr = {}) -> db.Model:
+    """Retrieves the models matching the corresponding attributes.
+    
+    :param table_class: SQLAlchemy class to get data from
+    :param attr: Dictionary of filters to use.
+    Empty dict returns the whole table.
+    
+    e.g. Passing in (Users, {"first_name":"John", "last_name":"Doe"})
+    generates the SQL statement
+    
+    select *
+    in users
+    where first_name = 'John'
+    and last_name = 'Doe'
+    
+    :return: List of all SQLAlchemy objects matching the filter."""
+    with app.app_context():
+        return table_class.query.filter_by(**attr).all()
+
+def get_col_names(table_class: Type[db.Model]) -> list[str]:
+    """Retrieves column names for a table.
+    
+    :param table_class: SQLAlchemy class to get data from
+    
+    :return: List of column names."""
+    with app.app_context():
+        return table_class.__table__.columns.keys()
+
+# def login(net_id, pw):
+#     """Logs in a user with the requested username and password.
+    
+#     :param net_id: NYU NetID used to login
+#     :type net_id: str
+#     :param pw: User password
+#     :type pw: pw
+    
+#     :return: Token on successful authentication, None otherwise
+#     """
+#     user = get_with_attribute(Users, {"net_id": net_id, "password": pw})
+#     if user != None:
+#         True
+    
+#     return None
+
+def delete(table_class: Type[db.Model], pk: any, commit = True) -> db.Model:
+    """Deletes model from its respective table via its primary key.
+    
+    :param table_class: SQLAlchemy class to get data from
+    :param pk: Primary key used to find desired row.
+    :param commit: Pushes changes immediately to database if True.
+    
+    :return: Deleted SQLAlchemy model if found, None otherwise
+    """
+    data = get_with_pk(table_class, pk)
+    with app.app_context():
+        if data != None:
+            db.session.delete(data)
+
+        if commit:
+            db.session.commit()
+
+    return data
+
+def run_raw_sql(statement: str) -> list:
+    """Runs the given SQL statement. Use as sparingly as possible,
+    as this does not work with the SQLAlchemy models, but rather
+    returns lists of tuples. No error checking is done here,
+    and inputs are not sanitized (yet?), so run at your own risk.
+    
+    :param statement: SQL statement to run.
+    :type statement: str
+    
+    :return: List of results of SQL statement.
+    """
+    with app.app_context():
+        print(db.session.execute(db.text(statement)).all())
+
+def commit():
+    """Saves all changes to the database.
+    
+    :return: None
+    """
+    with app.app_context():
+        db.session.commit()
+
 def test_db():
     with app.app_context():
+        # drop_all_tables()
         create_tables()
+        for i in range(1, 10):
+            p = Profiles(**{"net_id": "asdf" + str(i),
+                        "first_name": "first" + str(i),
+                        "last_name": "last" + str(i)})
+            u = Users(**{"net_id": "asdf" + str(i),
+                    "password": "pw" + str(i),
+                    "profile": p.net_id})
+            add(p, overwrite=True)
+            add(u, overwrite=True)
+
+    # print(get_with_attribute(Users, {"net_id":"asdf1", "password":"pw1"}))
+    # print(get_with_pk(Users, "asdf1"))
+    # print(type(get_with_pk(Users, "asdf1")))
+    
+    # print(delete(Users, "asdf1"))
+    # print(get_with_attribute(Users, {"net_id":"asdf1", "password":"pw1"}))
+    # print(get_with_pk(Users, "asdf1"))
+    # print(get_with_attribute(Users))
+    # print(Users.__dict__)
+    
+    # user = get_with_pk(Users, "asdf1")
+    # cols = get_col_names(Users)
+    # for col in cols:
+    #     print(type(user.__dict__))
+    # run_raw_sql("select * from users")
+    
+    # login("asdf2", "pw2")
 
 # Just for testing
-# if __name__ == '__main__':
-#     app = Flask(__name__)
-#     app.config['SQLALCHEMY_DATABASE_URI'] = \
-#         'postgresql://postgres:opendoor@localhost:5432/postgres'
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#     db.init_app(app)
-#     test_db()
+if __name__ == '__main__':
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'postgresql://postgres:opendoor@localhost:5432/postgres'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    
+    test_db()
