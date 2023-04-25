6ef955988b0fea4aeb2311240befbca1b65607b5
David Son <55555210+sondavidb@users.noreply.github.com>
2023-04-25 17:10:00 -0400

    Added user authentication (#36)

--- od_app/__init__.py
+++ od_app/__init__.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgresql://postgres:opendoor@localhost:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "wzb9Sp@WCn!3t4Jy" #For login tokens
 
 db = SQLAlchemy(app)
