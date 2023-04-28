16279093d0e71a24729b7e3866b5652ba029c7e9
juanjomoreno11 <castanomorenoj@gmail.com>
2023-04-28 19:42:32 -0400

    Now students can create events

--- app.py
+++ app.py
@@ -1,117 +1,150 @@
 from flask import render_template, redirect, request, url_for
 from od_app import app, db
 import threading
+from od_app.od_utils import db_utils
 from od_app.od_utils.merging import activities_merge
 
+
 def check_main_tabs():
-    if request.form.get('my_profile') == 'My Profile':
-        return 'profile_page'
+    if request.form.get("my_profile") == "My Profile":
+        return "profile_page"
 
-    elif request.form.get('student_events') == 'Student Events':
-        return 'student_events_page'
+    elif request.form.get("student_events") == "Student Events":
+        return "student_events_page"
 
-    elif request.form.get('fun_spots') == 'Fun Spots':
-        return 'fun_spots_page'
+    elif request.form.get("fun_spots") == "Fun Spots":
+        return "fun_spots_page"
 
-    elif request.form.get('happening_in_nyc') == 'Happening in NYC':
-        return 'happening_in_nyc_page'
+    elif request.form.get("happening_in_nyc") == "Happening in NYC":
+        return "happening_in_nyc_page"
     else:
         return False
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home_page():
-    if request.method == 'POST':
+    if request.method == "POST":
         tabs = check_main_tabs()
         if tabs:
             return redirect(url_for(tabs))
     else:
-        return render_template('home.html')
+        return render_template("home.html")
 
 
-@app.route('/myprofile', methods=["GET", "POST"])
+@app.route("/myprofile", methods=["GET", "POST"])
 def profile_page():
-    if request.method == 'POST':
+    if request.method == "POST":
         tabs = check_main_tabs()
         if tabs:
             return redirect(url_for(tabs))
-        elif request.form.get('Edit') == 'Edit':
-            return redirect(url_for('edit_profile_page'))
+        elif request.form.get("Edit") == "Edit":
+            return redirect(url_for("edit_profile_page"))
     else:
-        return render_template('profile.html', preferred_name="B42", major="Computer Science",
-                               dorm="Off-campus", full_name="Team B42", email="teamB42@teamB42.com",
-                               phone="(097) 234-5678", about_me="We are just CS students trying to graduate")
+        return render_template(
+            "profile.html",
+            preferred_name="B42",
+            major="Computer Science",
+            dorm="Off-campus",
+            full_name="Team B42",
+            email="teamB42@teamB42.com",
+            phone="(097) 234-5678",
+            about_me="We are just CS students trying to graduate",
+        )
 
 
-@app.route('/editprofile', methods=["GET", "POST"])
+@app.route("/editprofile", methods=["GET", "POST"])
 def edit_profile_page():
-    if request.method == 'POST':
-        if request.form.get('Cancel') == 'Cancel':
-            return redirect(url_for('profile_page'))
-        elif request.form.get('Save Profile') == 'Save Profile':
+    if request.method == "POST":
+        if request.form.get("Cancel") == "Cancel":
+            return redirect(url_for("profile_page"))
+        elif request.form.get("Save Profile") == "Save Profile":
             # Here is where all the backend for storing new profile data should go
-            return redirect(url_for('profile_page'))
+            return redirect(url_for("profile_page"))
     else:
-        return render_template('profile_form.html')
+        return render_template("profile_form.html")
 
 
-@app.route('/studentevents', methods=["GET", "POST"])
+@app.route("/studentevents", methods=["GET", "POST"])
 def student_events_page():
-    if request.method == 'POST':
+    if request.method == "POST":
         tabs = check_main_tabs()
         if tabs:
             return redirect(url_for(tabs))
-        elif request.form.get('see_rsvp_list') == 'See RSVP List':
-            return redirect(url_for('rsvp_list_page'))
-        elif request.form.get('New') == 'New':
-            return redirect(url_for('new_event_page'))
+        elif request.form.get("see_rsvp_list") == "See RSVP List":
+            return redirect(url_for("rsvp_list_page"))
+        elif request.form.get("New") == "New":
+            return redirect(url_for("new_event_page"))
     else:
-        return render_template('student_events.html', num_events=5)
+        return render_template("student_events.html", num_events=5)
 
 
-@app.route('/newevent', methods=["GET", "POST"])
+@app.route("/newevent", methods=["GET", "POST"])
 def new_event_page():
-    if request.method == 'POST':
-        return redirect(url_for('student_events_page'))  # This will need to be changed to whatever the POST is
+    if request.method == "POST":
+        if request.form.get("Create New Event") == "Create New Event":
+            event = {
+                "net_id": "JuanSupremacy",
+                "title": request.form["title"],
+                "place": request.form["location"],
+                "description": request.form["description"],
+                "date": request.form["date"],
+                "time": request.form["time"],
+                "fee": request.form["fee"],
+                "url": request.form["url"],
+                "reservation_needed": False,
+            }
+            db_utils.StudentEvents(**event).add_to_activities()
+
+        return redirect(
+            url_for("student_events_page")
+        )  # This will need to be changed to whatever the POST is
     else:
-        return render_template('new_event.html')
+        return render_template("new_event.html")
 
 
-@app.route('/funspots', methods=["GET", "POST"])
+@app.route("/funspots", methods=["GET", "POST"])
 def fun_spots_page():
-    if request.method == 'POST':
+    if request.method == "POST":
         tabs = check_main_tabs()
         if tabs:
             return redirect(url_for(tabs))
     else:
-        return render_template('fun_spots.html', num_spots=4)
+        return render_template("fun_spots.html", num_spots=4)
 
 
-@app.route('/happeninginnyc', methods=["GET", "POST"])
+@app.route("/happeninginnyc", methods=["GET", "POST"])
 def happening_in_nyc_page():
-    if request.method == 'POST':
+    if request.method == "POST":
         tabs = check_main_tabs()
         if tabs:
             return redirect(url_for(tabs))
     else:
-        return render_template('happening_in_nyc.html')
+        return render_template("happening_in_nyc.html")
 
 
-@app.route('/rsvplist', methods=["GET", "POST"])
+@app.route("/rsvplist", methods=["GET", "POST"])
 def rsvp_list_page():
-    if request.method == 'POST':
-        if request.form.get('Back') == 'Back':
-            return redirect(url_for('student_events_page'))
+    if request.method == "POST":
+        if request.form.get("Back") == "Back":
+            return redirect(url_for("student_events_page"))
     else:
-        return render_template('rsvp_list.html', event_name="The Hike", preferred_name="B42", major="Computer Science",
-                               dorm="Off-campus", full_name="Team B42", email="teamB42@teamB42.com",
-                               phone="(097) 234-5678")
+        return render_template(
+            "rsvp_list.html",
+            event_name="The Hike",
+            preferred_name="B42",
+            major="Computer Science",
+            dorm="Off-campus",
+            full_name="Team B42",
+            email="teamB42@teamB42.com",
+            phone="(097) 234-5678",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # ------Activities Merge Thread------------
     activities_load = threading.Thread(target=activities_merge)
     activities_load.start()
     # -----------------------------------------
-
+    # db_utils.drop_all_tables()
+    # db_utils.create_tables()
     app.run()
--- od_app/od_utils/db_utils.py
+++ od_app/od_utils/db_utils.py
@@ -18,9 +18,10 @@ import uuid
 from od_app import db
 from od_app import app
 
+
 class Profiles(db.Model):
     __tablename__ = "profiles"
-    
+
     net_id = db.Column(db.String(10), primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     preferred_name = db.Column(db.String(50))
@@ -30,14 +31,13 @@ class Profiles(db.Model):
     major = db.Column(db.String(50))
     description = db.Column(db.String(2000))
 
+
 class Users(db.Model):
     __tablename__ = "users"
-    
+
     net_id = db.Column(db.String(10), primary_key=True)
     password = db.Column(db.String(100), nullable=False)
-    profile = db.Column(db.String(10),
-                        db.ForeignKey("profiles.net_id"),
-                        nullable = False)
+    profile = db.Column(db.String(10), db.ForeignKey("profiles.net_id"), nullable=False)
 
     # Code stolen from
     # https://github.com/realpython/flask-jwt-auth/blob/d252dd88c7271580cbebc24c54f0259779123537/project/server/models.py#L28
@@ -48,15 +48,12 @@ class Users(db.Model):
         """
         try:
             payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-                'iat': datetime.datetime.utcnow(),
-                'sub': net_id
+                "exp": datetime.datetime.utcnow()
+                + datetime.timedelta(days=0, seconds=5),
+                "iat": datetime.datetime.utcnow(),
+                "sub": net_id,
             }
-            return jwt.encode(
-                payload,
-                app.config.get('SECRET_KEY'),
-                algorithm='HS256'
-            )
+            return jwt.encode(payload, app.config.get("SECRET_KEY"), algorithm="HS256")
         except Exception as e:
             return e
 
@@ -68,16 +65,17 @@ class Users(db.Model):
         :return: integer|string
         """
         try:
-            payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
-            return payload['sub']
+            payload = jwt.decode(auth_token, app.config.get("SECRET_KEY"))
+            return payload["sub"]
         except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
+            return "Signature expired. Please log in again."
         except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
+            return "Invalid token. Please log in again."
+
 
 class Activities(db.Model):
     __tablename__ = "activities"
-    
+
     activity_id = db.Column(db.Uuid, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     place = db.Column(db.String(255))
@@ -90,6 +88,7 @@ class Activities(db.Model):
     source = db.Column(db.String(63), nullable=False)
     rsvp_list = db.Column(db.ARRAY(db.String(10)))
 
+
 class NYUEvents(db.Model):
     __tablename__ = "nyu_events"
     id = db.Column(db.String(50), primary_key=True)
@@ -101,6 +100,7 @@ class NYUEvents(db.Model):
     description = db.Column(db.String(5000), nullable=True)
     fee = db.Column(db.Integer, nullable=True)
 
+
 class Ticketmaster(db.Model):
     __tablename__ = "ticketmaster"
 
@@ -110,55 +110,56 @@ class Ticketmaster(db.Model):
     time = db.Column(db.Time, nullable=False)
     img_url = db.Column(db.String(255))
     url = db.Column(db.String(255), nullable=False)
-    
+
+
 class StudentEvents(db.Model):
     __tablename__ = "student_events"
-    
-    net_id = db.Column(db.String(10), primary_key = True)
-    title = db.Column(db.String(255), nullable = False)
-    place = db.Column(db.String(127), nullable = False)
+
+    net_id = db.Column(db.String(10), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    place = db.Column(db.String(127), nullable=False)
     description = db.Column(db.String(2000))
     fee = db.Column(db.Integer)
     url = db.Column(db.String(255))
-    date = db.Column(db.Date, nullable = False)
-    time = db.Column(db.Time, nullable = False)
-    reservation_needed = db.Column(db.Boolean, nullable = False)
-    
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    reservation_needed = db.Column(db.Boolean, nullable=False)
+
     def add_to_activities(self):
         attr = {col.name: getattr(self, col.name) for col in self.__table__.columns}
-        date = attr['date']
-        time = attr['time']
-        net_id = attr['net_id']
-        attr.pop('date')
-        attr.pop('time')
-        attr.pop('net_id')
-        
-        attr['source'] = "student_events"
-        attr['datetime'] = date + " " + time
+        date = attr["date"]
+        time = attr["time"]
+        net_id = attr["net_id"]
+        attr.pop("date")
+        attr.pop("time")
+        attr.pop("net_id")
+
+        attr["source"] = "student_events"
+        attr["datetime"] = date + " " + time
         curr_time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-        hash_str = net_id + attr['title'] + attr['place'] + \
-                    date + time + curr_time
-        hash_md5 = hashlib.md5(bytes(hash_str, 'utf-8')).hexdigest()
-        attr['activity_id'] = uuid.UUID(hash_md5)
-        
+        hash_str = net_id + attr["title"] + attr["place"] + date + time + curr_time
+        hash_md5 = hashlib.md5(bytes(hash_str, "utf-8")).hexdigest()
+        attr["activity_id"] = uuid.UUID(hash_md5)
+
         add(Activities(**attr))
-        
-        
+
+
 def create_tables():
     """Ignores any conflicts with existing tables.
     Bear in mind any tables with the same name will not be made.
     Any table structure changes must be done via the database's CLI.
     Alternatively, drop all tables and recreate them.
-    
+
     return: None
     """
     with app.app_context():
         db.create_all()
 
+
 def drop_all_tables():
     """USE THIS SPARINGLY NEVER USE THIS IN PRODUCTION
     (So why did I make this? I dunno...)
-    
+
     return: None
     """
     tables = [Users, Ticketmaster, StudentEvents, Activities, Profiles, NYUEvents]
@@ -170,14 +171,15 @@ def drop_all_tables():
                 table.__table__.drop(db.engine)
         db.session.commit()
 
+
 def add(data: db.Model, commit=True, overwrite=False):
     """Inserts model into its respective table.
-    
+
     :param data: SQLAlchemy object to be inserted
     :param commit: Pushes changes immediately to database if True.
     :param overwrite: If True, overwrite existing
     data if primary key is found in table
-    
+
     :return: None
     """
     with app.app_context():
@@ -189,67 +191,72 @@ def add(data: db.Model, commit=True, overwrite=False):
         if commit:
             db.session.commit()
 
+
 def get_with_pk(table_class: Type[db.Model], pk: any) -> db.Model:
     """Retrieves the single model corresponding to the primary key.
-    
+
     :param table_class: SQLAlchemy class to get data from
     :param pk: Primary key used to find desired row.
-    
+
     :return: SQLAlchemy model if found, None otherwise."""
     with app.app_context():
         return db.session.get(table_class, pk)
 
-def get_with_attributes(table_class: Type[db.Model], attr = {}) -> db.Model:
+
+def get_with_attributes(table_class: Type[db.Model], attr={}) -> db.Model:
     """Retrieves the models matching the corresponding attributes.
-    
+
     :param table_class: SQLAlchemy class to get data from
     :param attr: Dictionary of filters to use.
     Empty dict returns the whole table.
-    
+
     e.g. Passing in (Users, {"first_name":"John", "last_name":"Doe"})
     generates the SQL statement
-    
+
     select *
     in users
     where first_name = 'John'
     and last_name = 'Doe'
-    
+
     :return: List of all SQLAlchemy objects matching the filter."""
     with app.app_context():
         return table_class.query.filter_by(**attr).all()
 
+
 def get_col_names(table_class: Type[db.Model]) -> list[str]:
     """Retrieves column names for a table.
-    
+
     :param table_class: SQLAlchemy class to get data from
-    
+
     :return: List of column names."""
     with app.app_context():
         return table_class.__table__.columns.keys()
 
+
 def login(net_id, pw):
     """Logs in a user with the requested username and password.
-    
+
     :param net_id: NYU NetID used to login
     :type net_id: str
     :param pw: User password
     :type pw: pw
-    
+
     :return: Token on successful authentication, None otherwise
     """
     user = get_with_attributes(Users, {"net_id": net_id, "password": pw})
     if user != None:
         return user[0].encode_auth_token(net_id)
-    
+
     return None
 
-def delete(table_class: Type[db.Model], pk: any, commit = True) -> db.Model:
+
+def delete(table_class: Type[db.Model], pk: any, commit=True) -> db.Model:
     """Deletes model from its respective table via its primary key.
-    
+
     :param table_class: SQLAlchemy class to get data from
     :param pk: Primary key used to find desired row.
     :param commit: Pushes changes immediately to database if True.
-    
+
     :return: Deleted SQLAlchemy model if found, None otherwise
     """
     data = get_with_pk(table_class, pk)
@@ -262,17 +269,18 @@ def delete(table_class: Type[db.Model], pk: any, commit = True) -> db.Model:
 
     return data
 
-def run_raw_sql(statement: str, get_output: bool=False):
+
+def run_raw_sql(statement: str, get_output: bool = False):
     """Runs the given SQL statement. Use as sparingly as possible,
     as this does not work with the SQLAlchemy models, but rather
     returns lists of tuples. No error checking is done here,
     and inputs are not sanitized (yet?), so run at your own risk.
-    
+
     :param statement: SQL statement to run.
     :param get_output: True if the output should be returned.
     If the query does not return rows, setting get_output=True will raise an exception
     :type statement: str
-    
+
     :return: List of results of SQL statement if get_output is True
     """
     res = None
@@ -289,51 +297,62 @@ def run_raw_sql(statement: str, get_output: bool=False):
 
 def commit():
     """Saves all changes to the database.
-    
+
     :return: None
     """
     with app.app_context():
         db.session.commit()
+
 
 def test_db():
     with app.app_context():
         # drop_all_tables()
         create_tables()
         for i in range(1, 10):
-            p = Profiles(**{"net_id": "asdf" + str(i),
-                        "first_name": "first" + str(i),
-                        "last_name": "last" + str(i)})
-            u = Users(**{"net_id": "asdf" + str(i),
+            p = Profiles(
+                **{
+                    "net_id": "asdf" + str(i),
+                    "first_name": "first" + str(i),
+                    "last_name": "last" + str(i),
+                }
+            )
+            u = Users(
+                **{
+                    "net_id": "asdf" + str(i),
                     "password": "pw" + str(i),
-                    "profile": p.net_id})
+                    "profile": p.net_id,
+                }
+            )
             add(p, overwrite=True)
             add(u, overwrite=True)
 
     # print(get_with_attribute(Users, {"net_id":"asdf1", "password":"pw1"}))
     # print(get_with_pk(Users, "asdf1"))
     # print(type(get_with_pk(Users, "asdf1")))
-    
+
     # print(delete(Users, "asdf1"))
     # print(get_with_attribute(Users, {"net_id":"asdf1", "password":"pw1"}))
     # print(get_with_pk(Users, "asdf1"))
     # print(get_with_attribute(Users))
     # print(Users.__dict__)
-    
+
     # user = get_with_pk(Users, "asdf1")
     # cols = get_col_names(Users)
     # for col in cols:
     #     print(type(user.__dict__))
     # run_raw_sql("select * from users")
-    
+
     # login("asdf2", "pw2")
 
+
 # Just for testing
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'postgresql://postgres:opendoor@localhost:5432/postgres'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = "wzb9Sp@WCn!3t4Jy" #For login tokens
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgresql://postgres:opendoor@localhost:5432/postgres"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = "wzb9Sp@WCn!3t4Jy"  # For login tokens
     db.init_app(app)
-    
+
     test_db()
